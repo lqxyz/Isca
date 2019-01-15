05d9643ddf2734ba316fcb3d7c5c524c9d54dd00
lqxyz <liuqunxyz@gmail.com>
2019-01-15 18:55:11 +0000

    delete some blank lines and spaces

--- src/atmos_column/column_initialize_fields.F90
+++ src/atmos_column/column_initialize_fields.F90
@@ -22,11 +22,8 @@
 module column_initialize_fields_mod
 
 use              fms_mod, only: mpp_pe, mpp_root_pe, write_version_number
-
 use      column_grid_mod, only: area_weighted_global_mean
-
 use        constants_mod, only: rdgas
-
 use       transforms_mod, only: get_grid_domain
 
 implicit none
@@ -42,51 +39,48 @@ contains
 !-------------------------------------------------------------------------------------------------
 subroutine column_initialize_fields(reference_sea_level_press, initial_temperature, surface_wind, &
                                     surf_geopotential, psg, ug, vg, tg)
+  real,    intent(in) :: reference_sea_level_press
+  real,    intent(in) :: initial_temperature
+  real,    intent(in) :: surface_wind
 
-real,    intent(in) :: reference_sea_level_press
-real,    intent(in) :: initial_temperature
-real,    intent(in) :: surface_wind
+  real,    intent(in),  dimension(:,:    ) :: surf_geopotential
+  real,    intent(out), dimension(:,:    ) :: psg
+  real,    intent(out), dimension(:,:,:  ) :: ug, vg, tg
 
-real,    intent(in),  dimension(:,:    ) :: surf_geopotential
-real,    intent(out), dimension(:,:    ) :: psg
-real,    intent(out), dimension(:,:,:  ) :: ug, vg, tg
+  real, allocatable, dimension(:,:) :: ln_psg
 
-real, allocatable, dimension(:,:) :: ln_psg
+  real :: initial_sea_level_press, global_mean_psg
+  real :: initial_perturbation   = 1.e-7
 
-real :: initial_sea_level_press, global_mean_psg
-real :: initial_perturbation   = 1.e-7
+  integer :: is, ie, js, je, num_levels
 
-integer :: is, ie, js, je, num_levels
+  call write_version_number(version, tagname)
 
-call write_version_number(version, tagname)
+  num_levels = size(ug,3)
+  call get_grid_domain(is, ie, js, je)
+  allocate(ln_psg(is:ie, js:je))
 
-num_levels = size(ug,3)
-call get_grid_domain(is, ie, js, je)
-allocate(ln_psg(is:ie, js:je))
+  initial_sea_level_press = reference_sea_level_press
 
-initial_sea_level_press = reference_sea_level_press  
+  ug      = 0.
+  vg      = 0.
+  tg      = 0.
+  psg     = 0.
 
-ug      = 0.
-vg      = 0.
-tg      = 0.
-psg     = 0.
+  ug(:,:,num_levels) = surface_wind / sqrt(2.)
+  vg(:,:,num_levels) = surface_wind / sqrt(2.)
 
-ug(:,:,num_levels) = surface_wind / sqrt(2.)
-vg(:,:,num_levels) = surface_wind / sqrt(2.) 
+  tg     = initial_temperature
+  ln_psg = log(initial_sea_level_press) - surf_geopotential/(rdgas*initial_temperature)
+  psg    = exp(ln_psg)
 
-tg     = initial_temperature
-ln_psg = log(initial_sea_level_press) - surf_geopotential/(rdgas*initial_temperature)
-psg    = exp(ln_psg)
+  ! compute and print mean surface pressure
+  global_mean_psg = area_weighted_global_mean(psg)
+  if(mpp_pe() == mpp_root_pe()) then
+    print '("mean surface pressure=",f9.4," mb")',.01*global_mean_psg
+  endif
 
-
-!  compute and print mean surface pressure
-global_mean_psg = area_weighted_global_mean(psg)
-if(mpp_pe() == mpp_root_pe()) then
-  print '("mean surface pressure=",f9.4," mb")',.01*global_mean_psg
-endif
-
-return
+  return
 end subroutine column_initialize_fields
-!================================================================================
 
 end module column_initialize_fields_mod
