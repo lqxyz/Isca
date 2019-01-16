9506bbc82d45c328246f03ab6450cd51ad894c1e
Qun Liu <liuqunxyz@gmail.com>
2019-01-16 16:03:23 +0000

    test rrtm single column model with or without ozone

--- exp/test_cases/column_test_case/column_test_rrtm_no_ozone.py
+++ exp/test_cases/column_test_case/column_test_rrtm_no_ozone.py
@@ -2,19 +2,18 @@ import os
 import numpy as np
 from isca import ColumnCodeBase, DiagTable, Experiment, Namelist, GFDL_BASE
 
-### to create ozone file:
-from scm_interp_routine import scm_interp, global_average_lat_lon
-
 # column model only uses 1 core
 NCORES = 1
 
-# compile code
+# compile code 
 base_dir = os.path.dirname(os.path.realpath(__file__))
 cb = ColumnCodeBase.from_directory(GFDL_BASE)
-cb.compile()
+cb.compile() 
 
 # create an Experiment object to handle the configuration of model parameters
-exp = Experiment('column_test_rrtm_ozone', codebase=cb)
+exp = Experiment('column_rrtm_no_ozone', codebase=cb)
+
+#exp.inputfiles = [os.path.join(GFDL_BASE,'input/rrtm_input_files/ozone_1990.nc')]
 
 #Tell model how to write diagnostics
 diag = DiagTable()
@@ -31,10 +30,11 @@ diag.add_field('column', 'sphum', time_avg=True)
 diag.add_field('column', 'ucomp', time_avg=True)
 diag.add_field('column', 'vcomp', time_avg=True)
 diag.add_field('column', 'temp', time_avg=True)
+diag.add_field('column', 'height', time_avg=True)
 diag.add_field('rrtm_radiation', 'toa_sw', time_avg=True)
 diag.add_field('rrtm_radiation', 'olr', time_avg=True)
 diag.add_field('rrtm_radiation', 'coszen', time_avg=True)
-diag.add_field('rrtm_radiation', 'ozone', time_avg=True)
+#diag.add_field('rrtm_radiation', 'ozone', time_avg=True)
 diag.add_field('atmosphere', 'dt_ug_diffusion', time_avg=True)
 diag.add_field('atmosphere', 'dt_vg_diffusion', time_avg=True)
 
@@ -44,78 +44,76 @@ exp.diag_table = diag
 exp.clear_rundir()
 
 #Define values for the 'core' namelist
-exp.namelist = Namelist({
+exp.namelist = namelist = Namelist({
     'main_nml':{
-     'days'   : 360,
+     'days'   : 30,
      'hours'  : 0,
      'minutes': 0,
      'seconds': 0,
-     'dt_atmos':7200,
+     'dt_atmos':360,
      'current_date' : [1,1,1,0,0,0],
      'calendar' : 'thirty_day'
     },
-
+    
     'atmosphere_nml': {
         'idealized_moist_model': True
     },
 
     'column_nml': {
         'lon_max': 1, # number of columns in longitude, default begins at lon=0.0
-        'lat_max': 1, # number of columns in latitude, precise
-                      # latitude can be set in column_grid_nml if only 1 lat used.
-        'num_levels': 50,  # number of levels
-        'initial_sphum': 1e-6,
-        'vert_coord_option': 'even_sigma'
+        'lat_max': 1, # number of columns in latitude, precise 
+                      # latitude can be set in column_grid_nml if only 1 lat used. 
+        'num_levels': 50,  # number of levels 
+        'initial_sphum': 1e-6, 
     },
 
-    'column_grid_nml': {
-        #'lat_value': np.rad2deg(np.arcsin(1/np.sqrt(3))) # set latitude to that which causes insolation in frierson p2 radiation to be insolation / 4.
-        'global_average': True # don't use this option at the moment
+    'column_grid_nml': { 
+        'lat_value': np.rad2deg(np.arcsin(1/np.sqrt(3))) # set latitude to that which causes insolation in frierson p2 radiation to be insolation / 4. 
+        #'global_average': True # don't use this option at the moment
     },
 
-    # set initial condition, NOTE: currently there is not an option to read in initial condition from a file.
+    # set initial condition, NOTE: currently there is not an option to read in initial condition from a file. 
     'column_init_cond_nml': {
-        'initial_temperature': 264., # initial atmospheric temperature
-        'surf_geopotential': 0.0, # applied to all columns
-        'surface_wind': 5. # as described above
+        'initial_temperature': 264., # initial atmospheric temperature 
+        'surf_geopotential': 0.0, # applied to all columns 
+        'surface_wind': 5. # as described above 
     },
 
     'idealized_moist_phys_nml': {
-        'do_damping': False, # no damping in column model, surface wind prescribed
+        'do_damping': False, # no damping in column model, surface wind prescribed 
         'turb':True,        # DONT WANT TO USE THIS, BUT NOT DOING SO IS STOPPING MIXED LAYER FROM WORKING
         'mixed_layer_bc':True, # need surface, how is this trying to modify the wind field? ****
-        'do_simple': True, # simple RH calculation
+        'do_simple': True, # simple RH calculation 
         'roughness_mom': 3.21e-05, # DONT WANT TO USE THIS, BUT NOT DOING SO IS STOPPING MIXED LAYER FROM WORKING
         'roughness_heat':3.21e-05,
-        'roughness_moist':3.21e-05,
+        'roughness_moist':3.21e-05,                
         'two_stream_gray': False,     #Use grey radiation
-        'do_rrtm_radiation': True,
-        'convection_scheme': 'SIMPLE_BETTS_MILLER', #Use the simple Betts Miller convection scheme
+        'convection_scheme': 'SIMPLE_BETTS_MILLER', #Use the simple Betts Miller convection scheme 
+        'do_rrtm_radiation': True,    #Use RRTM radiation, not grey
     },
 
     'rrtm_radiation_nml': {
         'solr_cnst': 1360,  #s set solar constant to 1360, rather than default of 1368.22
         'dt_rad': 7200, #Use long RRTM timestep
-        'do_rad_time_avg':True,
-        'dt_rad_avg':86400,
-        'co2ppmv':400.,
+        'do_read_ozone': False,
+        #'ozone_file': 'ozone_1990'
     },
 
     'qe_moist_convection_nml': {
-        'rhbm':0.7, # rh criterion for convection
-        'Tmin':160., # min temperature for convection scheme look up tables
-        'Tmax':350.  # max temperature for convection scheme look up tables
+        'rhbm':0.7, # rh criterion for convection 
+        'Tmin':160., # min temperature for convection scheme look up tables 
+        'Tmax':350.  # max temperature for convection scheme look up tables 
     },
-
+    
     'lscale_cond_nml': {
-        'do_simple':True, # only rain
-        'do_evap':False,  # no re-evaporation of falling precipitation
+        'do_simple':True, # only rain 
+        'do_evap':False,  # no re-evaporation of falling precipitation 
     },
 
     'surface_flux_nml': {
-        'use_virtual_temp': True, # use virtual temperature for BL stability
+        'use_virtual_temp': True, # use virtual temperature for BL stability 
         'do_simple': True,
-        'old_dtaudv': True
+        'old_dtaudv': True    
     },
 
     'vert_turb_driver_nml': { # DONT WANT TO USE THIS, BUT NOT DOING SO IS STOPPING MIXED LAYER FROM WORKING
@@ -128,15 +126,15 @@ exp.namelist = Namelist({
 
     #Use a large mixed-layer depth, and the Albedo of the CTRL case in Jucker & Gerber, 2017
     'mixed_layer_nml': {
-        'tconst' : 285.,
-        'prescribe_initial_dist':False,
-        'evaporation':True,
+        'tconst': 285.,
+        'prescribe_initial_dist': False,
+        'evaporation': True,
         'depth': 2.5,                          #Depth of mixed layer used
-        'albedo_value': 0.20,                  #Albedo value used
+        'albedo_value': 0.30,                  #Albedo value used             
     },
-
+    
     'sat_vapor_pres_nml': {
-        'do_simple':True,
+        'do_simple':True, 
     },
 
     # FMS Framework configuration
@@ -154,13 +152,6 @@ exp.namelist = Namelist({
 
 #Lets do a run!
 if __name__=="__main__":
-    ds = scm_interp(filename=os.path.join(GFDL_BASE,'input/rrtm_input_files/ozone_1990.nc'),
-               varname='ozone_1990',
-               nlevels=50)
-    global_average_lat_lon(ds, 'ozone_1990_interp')
-    exp.namelist['rrtm_radiation_nml']['do_scm_ozone'] = True
-    exp.namelist['rrtm_radiation_nml']['scm_ozone'] = np.squeeze(ds.ozone_1990_interp_area_av.mean('time').values).tolist()
-
     exp.run(1, use_restart=False, num_cores=NCORES, overwrite_data=False)
     for i in range(2, 121):
         exp.run(i, num_cores=NCORES, overwrite_data=False)
